a8dac24def4df69d81db9a23d1b794e1940c4d02
Charles Smith <darkgrey@subfix.co.uk>
2025-01-30 19:28:29 -0600

    hotfix

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-    const baseURL = process.env.NEXT_PUBLIC_API_URL || 'https://api.cftx.net/prod-cached';
+    const baseURL = process.env.NEXT_PUBLIC_API_URL || 'https://api.cftx.net';
 
     export const api = axios.create({
       baseURL,
